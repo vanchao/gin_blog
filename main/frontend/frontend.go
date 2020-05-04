629b585970d486afa5303f562d0e1e4fa8cdd31d
fanchao <van.chao@hotmail.com>
2020-05-04 10:13:25 +0800

    Separate router

---
+++ main/frontend/frontend.go
@@ -0,0 +1,99 @@
+package frontend
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
+	sq "github.com/Masterminds/squirrel"
+)
+
+type Post struct {
+	Id        int    `json:"id" form:"id"`
+	TagId     int    `json:"tag_id" form:"tag_id"`
+	AuthorId  int    `json:"author_id" form:"author_id"`
+	UpdatedTime sql.NullString      `json:"updated_time" form:"updated_time"`
+	CreatedTime string      `json:"created_time" form:"created_time"`
+	AuthorName string      `json:"author_name" form:"author_name"`
+	Title string `json:"title" form:"title"`
+	TagName string `json:"tag_name" form:"tag_name"`
+	Description string `json:"description" form:"description"`
+	Content string `json:"content" form:"content"`
+}
+
+
+func Post_list(c *gin.Context) {
+
+	var offset, limit uint64
+
+	offset	 = 0
+
+	limit 	 = 5
+
+	sql_string, args, err := sq.Select("*").From("posts").Offset(offset).Limit(limit).ToSql()
+
+	if(nil != err) {
+		e := fmt.Sprintf("Error found40: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error_msg": e,
+		})
+	}
+
+	sqlConnString := getConnString()
+	db, err := sql.Open("mysql", sqlConnString)
+	if err != nil {
+		e := fmt.Sprintf("Error found50: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error_msg": e,
+		})
+	}
+
+	defer db.Close()
+	rows, err := db.Query(sql_string)
+	if err != nil {
+		e := fmt.Sprintf("Error found60: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error_msg": e,
+		})
+	}
+
+	posts := make([]Post, 0)
+
+	for rows.Next() {
+		var post Post
+		rows.Scan(&post.Id, &post.TagId, &post.AuthorId, &post.UpdatedTime, &post.CreatedTime, &post.AuthorName, &post.Title, &post.TagName, &post.Description, &post.Content)
+		posts = append(posts, post)
+	}
+
+	_ = args
+
+	if(nil != err) {
+		e := fmt.Sprintf("Error found70: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error_msg": e,
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": posts,
+	})
+
+}
+
+func getConnString() string {
+	viper.SetConfigName("database")
+	viper.SetConfigType("json")
+	viper.AddConfigPath("./main/config/")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	uname := viper.Get("user_name")
+	pwd := viper.Get("password")
+	protocol := viper.Get("protocol")
+	db := viper.Get("database")
+	str := fmt.Sprintf("%s:%s@%s/%s", uname, pwd, protocol, db)
+	return str
+}
+
